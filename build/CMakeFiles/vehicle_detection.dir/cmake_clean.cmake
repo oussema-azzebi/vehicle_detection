0e2a989267c60dd1bf93883913ce6226c7abc218
Poka12 <35254967+oussema-azzebi@users.noreply.github.com>
2020-05-07 19:34:45 +0200

    Add files via upload

---
+++ build/CMakeFiles/vehicle_detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vehicle_detection.dir/src/main.cpp.o"
+  "CMakeFiles/vehicle_detection.dir/src/tools.cpp.o"
+  "vehicle_detection.pdb"
+  "vehicle_detection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vehicle_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
